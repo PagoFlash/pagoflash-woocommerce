43ac9a27fde31a9b52333aa819a21e7cdcaf0673
argenisfd <argenisfd@gmail.com>
2016-07-22 13:49:16 -0400

    Agregando los archivos de nuevo

---
+++ libs/pagoflash-sdk/pagoflash_parameters.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+  * @author Argenis Fontalvo [argenisfd@gmail.com]
+  * @version 0.0.1
+  * 
+  * IMPORTANTE: No modifique los valores de este archivo   
+  *
+  */
+
+return array(
+	'prod'=>array(
+		'domain'=>"https://app.pagoflash.com"
+	),
+	'dev'=>array(
+		'domain'=>'http://app-test2.pagoflash.com'
+	)
+);
